10b52ccfa971f43bba5b83984cf64fb2098948b8
Du Fei <fei.du@nxp.com>
2016-11-15 20:29:32 +0800

    move windows powershell profile back

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,3 @@
-#Set-Alias gh Get-Help
-function set-profile {
-    ise $profile
-}
-
 function mad{
 start 'http://compass.freescale.net/livelink/livelink?func=ll&objId=194994268&objAction=browse&sort=name'
 }
@@ -14,6 +9,9 @@ start 'https://nxp1.sharepoint.com/sites/MCUops/baces/SitePages/Home.aspx'
 function compassultra{
  start 'http://compass.freescale.net/livelink/livelink?func=ll&objId=234218038&objAction=browse&sort=name'
 }
+function timesheet{
+start 'https://nxp.sciforma.net/sciforma/?ACSREQUESTID=_ecb55570-1049-4b5d-af18-03434f742b42#95403'
+}
 
 # using autohotkey to speed up
 C:\Users\b41395\Documents\WindowsPowerShell\autohotkey.ahk
@@ -23,7 +21,7 @@ C:\Users\b41395\Downloads\WindowsTrans\trans.exe -g $transid
 }
 
 function birthday{
-[datetime]'06/03/2016' - (Get-Date)
+[datetime]'06/03/2017' - (Get-Date)
 }
 
 function google($search) {
@@ -34,11 +32,12 @@ function youtube($search) {
 start "https://www.youtube.com/results?search_query=$search"
 }
 
+
 function stackoverflow($search) {
 start "http://stackoverflow.com/search?q=$search"
 }
 
-function pwdclip(){
+function pwdcp(){
 (pwd).Path | clip
 }
 
@@ -77,6 +76,14 @@ C:\Users\b41395\Downloads\WindowsTrans\CFImager.exe -raw -offset 0x400 -f .\imx6
 Pop-Location
 }
 
+function write-ubt{
+Push-Location
+cd C:\TEMP
+$localpath = pwd
+Get-SFTPFile -SFTPSession $Session -RemoteFile "/home/b41395/6ull/uboot-imx-testbuild/u-boot.imx" -LocalPath $localpath -Overwrite
+C:\Users\b41395\Downloads\WindowsTrans\CFImager.exe -raw -offset 0x400 -f .\u-boot.imx -d D -n
+Pop-Location
+}
 function Out-Default
 {
     [CmdletBinding(confirmimpact="medium")]
@@ -131,9 +138,8 @@ function Out-Default
     }
 }
 
-
 $host.PrivateData.Errorforegroundcolor = 'green'
-Start-Transcript .\console.txt -Append
+#Start-Transcript .\console.txt -Append
 
 function sdhc-write($transid){
     Set-Location C:\Users\b41395\Downloads\WindowsTrans\repo
@@ -141,8 +147,8 @@ function sdhc-write($transid){
         mv .\u-boot.imx ([string](Get-Date).Ticks + "u-boot.imx")
         ..\trans.exe -g $transid
         }
-    if ((Get-PSDrive e -ErrorAction Stop).free -lt 0) {
-    ..\CFImager.exe -raw -offset 0x400 -f .\u-boot.imx -d E -n
+    if ((Get-PSDrive d -ErrorAction Stop).free -lt 0) {
+    ..\CFImager.exe -raw -offset 0x400 -f .\u-boot.imx -d D -n
     }
 }
 
@@ -167,60 +173,141 @@ function stop-app{
     Get-Process i.MX6UL_Validation -ErrorAction SilentlyContinue| Stop-Process
 }
 
-function get-com5{
-C:\Users\b41395\Downloads\putty.exe -new_console:s -load "COM5"
+function open-com {
+    #C:\Users\b41395\Downloads\putty.exe -load "COM9" -new_console:s40H
+    C:\Users\b41395\Downloads\putty.exe  -new_console:s40H
+}
+function close-com {
+    ps putty | spps
 }
 
+function 18th {
+    C:\Users\b41395\Downloads\putty.exe -load "18th" -new_console
+}
+function get-com {
+    [System.IO.Ports.SerialPort]::GetPortNames()
+}
 
 Add-Type -AssemblyName System.Windows.Forms
 
-
-Set-Alias -Name ls -Value PowerLS -Option AllScope
+Set-Alias -Name zip -Value Compress-Archive
+Set-Alias -Name unzip -Value Expand-Archive
+#Set-Alias -Name ls -Value PowerLS -Option AllScope
 #Export-ModuleMember -Function *-* -Alias *
 
 
-#New-PSDrive -Name kl80 -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\Project\K80_256\IDD\KL80" | Out-Null
-#New-PSDrive -Name kl80repo -PSProvider FileSystem -Root "Z:\KL80\IDD" | Out-Null
-#New-PSDrive -Name pss -PSProvider FileSystem -Root "C:\Users\b41395\learning\powershell\scripting" | Out-Null
-#New-PSDrive -Name xml -PSProvider FileSystem -Root "C:\Users\b41395\learning\xml" | Out-Null
-#New-PSDrive -Name pi -PSProvider FileSystem -Root "C:\Users\b41395\learning\pi" | Out-Null
-#New-PSDrive -Name ma512 -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\Project\MA512" | Out-Null
-#New-PSDrive -Name mt256 -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\Project\MT256" | Out-Null
-#New-PSDrive -Name vim -PSProvider FileSystem -Root "C:\Users\b41395\learning\vim" | Out-Null
-
-#New-PSDrive -Name git -PSProvider FileSystem -Root "C:\Users\b41395\learning\git" | Out-Null
-#New-PSDrive -Name ks22 -PSProvider FileSystem -Root "C:\Users\b41395\OneDrive - Freescale Semiconductor\Projects\KS256" | Out-Null
-
-#New-PSDrive -Name ruby -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\learning\Ruby" | Out-Null
-
-#New-PSDrive -Name ks22i2cdut -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\Project\KS22\lpi2c\examples\mapsks22f256\demo_apps\lpi2c_slave_vlps\src" | Out-Null
-
-#New-PSDrive -Name imx -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\Project\I.MX" | Out-Null
-#New-PSDrive -Name arm -PSProvider FileSystem -Root "C:\Users\b41395\learning\arm" | Out-Null
-#New-PSDrive -Name r -PSProvider FileSystem -Root "C:\Users\b41395\D\Denny\BACES\learning\Ruby\r" | Out-Null
-#New-PSDrive -Name linux FileSystem -Root "C:\Users\b41395\D\Denny\BACES\learning\Linux" | Out-Null
-
-
-#$pdf='C:\Program Files (x86)\Adobe\Reader 11.0\Reader\AcroRd32.exe'
-# function Prompt {"Chulk. >"}
 New-Alias wheres Compare-Property
 
-$env:Path += ";C:\Program Files (x86)\Google\Chrome\Application"
-$env:Path += ";C:\Program Files\Internet Explorer"
+$env:Path += ";C:\Users\b41395\myexe\ctags58"
 $env:Path += ";C:\Program Files\R\R-3.2.3\bin\x64"
 $env:Path += "C:\Program Files\7-Zip"
 $env:Path += "C:\Users\b41395\Downloads\WindowsTrans"
-#$env:Path += ";C:\Program Files\LeostreamConnect\"
-#$env:Path += ";C:\Program Files (x86)\Tencent\WeChat\"
-#$env:Path += ";C:\Program Files (x86)\Vim\vim74\"
+$env:Path += "C:\Program Files\Far Manager"
+$env:Path += "C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\arm\bin"
+$env:Path += ";C:\Program Files (x86)\SEGGER\JLink_V502d"
+$env:Path += ";C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin"
 Enable-HistoryPersistence
 New-DynamicVariable GLOBAL:WindowTitle -Getter { $host.UI.RawUI.WindowTitle} -Setter {$host.ui.RawUI.WindowTitle = $args[0]}
 # Load posh-git example profile
 . 'C:\Users\b41395\Documents\WindowsPowerShell\Modules\posh-git\profile.example.ps1'
 
 
-# Load Jump-Location profile
+Load Jump-Location profile
 Import-Module 'C:\Users\b41395\Documents\WindowsPowerShell\Modules\Jump.Location\Jump.Location.psd1'
-Import-Module PSColors
+#Import-Module PSColors
 Import-Module PowerTab
 Import-Module PSReadLine
+
+Set-PSReadlineOption -EditMode vi
+#
+# Ctrl+Shift+j then type a key to mark the current directory.
+# Ctrj+j then the same key will change back to that directory without
+# needing to type cd and won't change the command line.
+
+#
+$global:PSReadlineMarks = @{}
+
+Set-PSReadlineKeyHandler -Key Ctrl+Shift+j `
+                         -BriefDescription MarkDirectory `
+                         -LongDescription "Mark the current directory" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $key = [Console]::ReadKey($true)
+    $global:PSReadlineMarks[$key.KeyChar] = $pwd
+}
+
+Set-PSReadlineKeyHandler -Key Ctrl+j `
+                         -BriefDescription JumpDirectory `
+                         -LongDescription "Goto the marked directory" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $key = [Console]::ReadKey()
+    $dir = $global:PSReadlineMarks[$key.KeyChar]
+    if ($dir)
+    {
+        cd $dir
+        [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+    }
+}
+
+Set-PSReadlineKeyHandler -Key Alt+j `
+                         -BriefDescription ShowDirectoryMarks `
+                         -LongDescription "Show the currently marked directories" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $global:PSReadlineMarks.GetEnumerator() | % {
+        [PSCustomObject]@{Key = $_.Key; Dir = $_.Value} } |
+        Format-Table -AutoSize | Out-Host
+
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+}
+
+Set-PSReadlineKeyHandler -Chord 'Oem7','Shift+Oem7' `
+                         -BriefDescription SmartInsertQuote `
+                         -LongDescription "Insert paired quotes if not already on a quote" `
+                         -ScriptBlock {
+    param($key, $arg)
+
+    $line = $null
+    $cursor = $null
+    [Microsoft.PowerShell.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+
+    if ($line[$cursor] -eq $key.KeyChar) {
+        # Just move the cursor
+        [Microsoft.PowerShell.PSConsoleReadline]::SetCursorPosition($cursor + 1)
+    }
+    else {
+        # Insert matching quotes, move cursor to be in between the quotes
+        [Microsoft.PowerShell.PSConsoleReadline]::Insert("$($key.KeyChar)" * 2)
+        [Microsoft.PowerShell.PSConsoleReadline]::GetBufferState([ref]$line, [ref]$cursor)
+        [Microsoft.PowerShell.PSConsoleReadline]::SetCursorPosition($cursor - 1)
+    }
+}
+Set-PSReadLineOption -HistorySearchCursorMovesToEnd 
+Set-PSReadlineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadlineKeyHandler -Key DownArrow -Function HistorySearchForward
+Set-PSReadlineKeyHandler -Key PageUp -Function ScrollDisplayUp
+Set-PSReadlineKeyHandler -Key PageDown -Function ScrollDisplayDown
+Set-PSReadlineKeyHandler -Key Ctrl+[ -Function ViCommandMode
+
+Remove-Module powershellcookbook
+
+$r ='C:\Program Files\R\R-3.3.1\bin\R.exe'
+
+function slexe{
+gci -Path C:\Users\b41395\proj\ssl\try_env\build\B41395\imx6sll *exe -Recurse | sort lastwritetime -Descending | select -First 1 | ii 
+}
+
+function Set-profile{
+	vim C:\Users\b41395\my_profile.ps1
+}
+
+set-alias gh get-help
+
+function Connect-Wifi{
+netsh wlan connect name=Freescale
+}
+set-alias wifi Connect-Wifi
--- autohotkey.ahk
+++ autohotkey.ahk
@@ -1,6 +1,6 @@
 #Persistent
 #SingleInstance force
-^m::Send, Df201613{Enter}
+^m::Send, Df201616{Enter}
 #n::
 Run, C:\Program Files (x86)\Google\Chrome\Application\chrome.exe
 WinWait, Untitled - Google Chrome, 
@@ -25,28 +25,134 @@ return
 RControl & RShift::AltTab  ; Hold down right-control then press right-shift repeatedly to move forward.
 RControl & Enter::ShiftAltTab  ; Without even having to release right-control, press Enter to reverse direction.
 
+
+#IfWinActive ahk_class AcrobatSDIWindow
+h:: 
+if (inAcrobatSearchMode)
+  Send h
+else Send {Left}
+return
+
+j::
+if (inAcrobatSearchMode)
+  Send j 
+else Send {Down}
+return
+
+k::
+if (inAcrobatSearchMode)
+  Send k 
+else Send {Up}
+return
+
+l::
+if (inAcrobatSearchMode)
+  Send l 
+else Send {Right}
+return
+
+n::
+if (inAcrobatSearchMode)
+  Send n
+else Send {F3}{Esc}
+return
+
++n::
+if (inAcrobatSearchMode)
+  Send N
+else Send +{F3}{Esc}
+return
+
++g::
+if (inAcrobatSearchMode)
+  Send G
+else Send {End} 
+return
+
+; see http://stackoverflow.com/questions/1794258/detect-a-double-key-press-in-autohotkey
+
+g::
+if (inAcrobatSearchMode)
+  Send g
+else {
+  if (A_PriorHotkey <> "g" or A_TimeSincePriorHotkey > 400) {
+      ; Too much time between presses, so this isn't a double-press.
+      KeyWait, g
+      return
+  }
+  Send {Home}
+}
+return
+
+/::
+if (inAcrobatSearchMode)
+  Send /
+else {
+  inAcrobatSearchMode := true
+  Send ^f
+}
+return
+
+Esc::
+inAcrobatSearchMode := false
+Send {Esc}
+return
+
+^[::
+inAcrobatSearchMode := false
+Send {Esc}
+return
+
+Enter::
+if (inAcrobatSearchMode) {
+  inAcrobatSearchMode := false
+}
+Send {Enter}
+return
+
+;go back into normal mode after scrolling with any control command
+
+^e::
+inAcrobatSearchMode := false
+Send {Esc}{Down}
+return
+
+^y::
+inAcrobatSearchMode := false
+Send {Esc}{Up}
+return
+
+^f::
+inAcrobatSearchMode := false
+Send {Esc}{PgDn}
+return
+
+^b::
+inAcrobatSearchMode := false
+Send {Esc}{PgUp}
+return
+
+#IfWinActive
+
 :*:/ip::
 	Send, %A_IPAddress1%
 return
-::6ul::/proj/imx6ul/design/workarea/b41395/
-::6ull::/proj/imx6ull/design/workarea/b41395/
-::wapp::write-app
-::wubt::sdhc-write
-::@wy::wanyuan.yu@nxp.com
+;::6ul::/proj/imx6ul/design/workarea/b41395/
+;::6ull::/proj/imx6ull/design/workarea/b41395/
 ::@df::fei.du@nxp.com
-::tjnpe::TJN PE Weekly ww1016 DF JJ
-::@team::Jianguang Cao <jianguang.cao@nxp.com>; Le Chang <le.chang@nxp.com>; Xin Cui <xin.cui@nxp.com>; Haifeng Hu <haifeng.hu@nxp.com>; Pengyan Li <pengyan.li@nxp.com>; Wen Li <winnie.li@nxp.com>; Zheyuan Liu <zheyuan.liu@nxp.com>; Bin Lu <bin.lu@nxp.com>; Kan Liu <kan.liu@nxp.com>; Ken Song <ken.song@nxp.com>; Siliang Wang <siliang.wang@nxp.com>; Zhicheng Wang <zhicheng.wang@nxp.com>; Li Jiang Wu <lijiang.wu@nxp.com>; Yunpeng Xu <yunpeng.xu@nxp.com>; Wanyuan Yu <wanyuan.yu@nxp.com>; Hongxu Zhang <hongxu.zhang@nxp.com>; Rixing Zhao <rixing.zhao@nxp.com>; Bin Wang <bin.wang@nxp.com>
-OnClipboardChange:
-ToolTip Clipboard data type: %A_EventInfo%
-a = %A_eventinfo%
-if (a = 2)
-{
-	send {lcontrol down}c{lcontrol up}
-	Sleep 100
-	Send {LControl down}``{LControl up}
-	send gps SnippingTool -ErrorAction SilentlyContinue | spps {Enter}
-	Send {LControl down}``{LControl up}
-}
+::tjnpe::TJN PE Weekly ww1016 DF JJ 
+::@team::Jianguang Cao <jianguang.cao@nxp.com>; Le Chang <le.chang@nxp.com>; Xin Cui <xin.cui@nxp.com>; Haifeng Hu <haifeng.hu@nxp.com>; Pengyan Li <pengyan.li@nxp.com>; Wen Li <winnie.li@nxp.com>; Zheyuan Liu <zheyuan.liu@nxp.com>; Bin Lu <bin.lu@nxp.com>; Kan Liu <kan.liu@nxp.com>; Ken Song <ken.song@nxp.com>; Siliang Wang <siliang.wang@nxp.com>; Zhicheng Wang <zhicheng.wang@nxp.com>; Li Jiang Wu <lijiang.wu@nxp.com>; Yunpeng Xu <yunpeng.xu@nxp.com>; Wanyuan Yu <wanyuan.yu@nxp.com>; Hongxu Zhang <hongxu.zhang@nxp.com>; Rixing Zhao <rixing.zhao@nxp.com>; Bin Wang <bin.wang@nxp.com>;Shawn Han <shawn.han@nxp.com>
+;OnClipboardChange:
+;ToolTip Clipboard data type: %A_EventInfo%
+;a = %A_eventinfo%
+;if (a = 2)
+;{
+	;send {lcontrol down}c{lcontrol up}
+	;Sleep 100
+	;Send {LControl down}``{LControl up}
+	;send gps SnippingTool -ErrorAction SilentlyContinue | spps {Enter}
+	;Send {LControl down}``{LControl up}
+;}
 Sleep 1000
 ToolTip  ; Turn off the tip.
 return
