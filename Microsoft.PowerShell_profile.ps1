b3441fcf471b27c40546f7033f6206edac9077ec
Du Fei <fei.du@nxp.com>
2016-11-17 19:55:44 +0800

    remove unused functions including u-boot, scp related

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,3 @@
-function mad{
-start 'http://compass.freescale.net/livelink/livelink?func=ll&objId=194994268&objAction=browse&sort=name'
-}
-
 function baceshome{
 start 'https://nxp1.sharepoint.com/sites/MCUops/baces/SitePages/Home.aspx'
 }
@@ -33,57 +29,10 @@ start "https://www.youtube.com/results?search_query=$search"
 }
 
 
-function stackoverflow($search) {
-start "http://stackoverflow.com/search?q=$search"
-}
-
 function pwdcp(){
 (pwd).Path | clip
 }
 
-function baidu($search){
-start "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=$search&rsv_pq=dd3d27a9005ae8b1&rsv_t=b3c43wiJzwoT02u%2Fo%2FB0RgW7Yn0nGHXqzcW35zirhzf6dYUH8e6tTwFQ4JY&rsv_enter=1&rsv_sug3=7&rsv_sug1=4&rsv_sug7=100"
-}
-function bing($search){
-"http://cn.bing.com/search?q=$search&go=Submit&qs=n&form=QBLH&pq=svn&sc=8-3&sp=-1&sk=&cvid=6241CF63D8FE4E5B96CFC198F441AC4E"
-}
-
-$credentials = Get-Credential -Credential b41395
-$Session = New-SFTPSession -ComputerName "lvd5192.ap.freescale.net" -Credential $credentials
-function update-elf{
-$localpath = "Y:\proj\imx6ull\design\workarea\b41395\test\validation\vectors\osc\stimulus"
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/osc.map" -LocalPath $localpath -Overwrite
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/osc.lst_lnk" -LocalPath $localpath -Overwrite
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/osc.elf.hex" -LocalPath $localpath -Overwrite
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/osc.elf" -LocalPath $localpath -Overwrite
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/osc.c" -LocalPath $localpath -Overwrite
-}
-
-function get-exe{
-Push-Location
-cd C:\TEMP
-$localpath = pwd
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/imx6ull_osc_osc.exe" -LocalPath $localpath -Overwrite
-.\imx6ull_osc_osc.exe -new_console:s
-Pop-Location
-}
-function write-bin{
-Push-Location
-cd C:\TEMP
-$localpath = pwd
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/proj/imx6ull/design/workarea/b41395/test/validation/vectors/osc/stimulus/imx6ull_osc_osc.bin" -LocalPath $localpath -Overwrite
-C:\Users\b41395\Downloads\WindowsTrans\CFImager.exe -raw -offset 0x400 -f .\imx6ull_osc_osc.bin -d D -n
-Pop-Location
-}
-
-function write-ubt{
-Push-Location
-cd C:\TEMP
-$localpath = pwd
-Get-SFTPFile -SFTPSession $Session -RemoteFile "/home/b41395/6ull/uboot-imx-testbuild/u-boot.imx" -LocalPath $localpath -Overwrite
-C:\Users\b41395\Downloads\WindowsTrans\CFImager.exe -raw -offset 0x400 -f .\u-boot.imx -d D -n
-Pop-Location
-}
 function Out-Default
 {
     [CmdletBinding(confirmimpact="medium")]
@@ -139,39 +88,7 @@ function Out-Default
 }
 
 $host.PrivateData.Errorforegroundcolor = 'green'
-#Start-Transcript .\console.txt -Append
 
-function sdhc-write($transid){
-    Set-Location C:\Users\b41395\Downloads\WindowsTrans\repo
-    if ([string]$transid.length -eq 9){
-        mv .\u-boot.imx ([string](Get-Date).Ticks + "u-boot.imx")
-        ..\trans.exe -g $transid
-        }
-    if ((Get-PSDrive d -ErrorAction Stop).free -lt 0) {
-    ..\CFImager.exe -raw -offset 0x400 -f .\u-boot.imx -d D -n
-    }
-}
-
-function write-app{
-Push-Location
-Set-Location C:\Users\b41395\Downloads\WindowsTrans\repo
-    if ((Get-PSDrive e -ErrorAction Stop).free -lt 0) {
-    Write-Host (Get-ChildItem *bin | sort -Descending | Select-Object -First 1).name
-    ..\CFImager.exe -raw -offset 0x400 -f (Get-ChildItem *bin | sort -Descending | Select-Object -First 1).fullname -d E -n
-    }
-Pop-Location
-}
-
-function start-app{
-Push-Location
-Set-Location C:\Users\b41395\Downloads\WindowsTrans\repo
-    C:\Users\b41395\Downloads\WindowsTrans\repo\i.MX6UL_Validation.exe
-Pop-Location
-}
-
-function stop-app{
-    Get-Process i.MX6UL_Validation -ErrorAction SilentlyContinue| Stop-Process
-}
 
 function open-com {
     #C:\Users\b41395\Downloads\putty.exe -load "COM9" -new_console:s40H
@@ -200,9 +117,7 @@ New-Alias wheres Compare-Property
 
 $env:Path += ";C:\Users\b41395\myexe\ctags58"
 $env:Path += ";C:\Program Files\R\R-3.2.3\bin\x64"
-$env:Path += "C:\Program Files\7-Zip"
 $env:Path += "C:\Users\b41395\Downloads\WindowsTrans"
-$env:Path += "C:\Program Files\Far Manager"
 $env:Path += "C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\arm\bin"
 $env:Path += ";C:\Program Files (x86)\SEGGER\JLink_V502d"
 $env:Path += ";C:\Program Files (x86)\IAR Systems\Embedded Workbench 7.5\common\bin"
@@ -214,7 +129,6 @@ New-DynamicVariable GLOBAL:WindowTitle -Getter { $host.UI.RawUI.WindowTitle} -Se
 
 Load Jump-Location profile
 Import-Module 'C:\Users\b41395\Documents\WindowsPowerShell\Modules\Jump.Location\Jump.Location.psd1'
-#Import-Module PSColors
 Import-Module PowerTab
 Import-Module PSReadLine
 
@@ -301,13 +215,13 @@ function slexe{
 gci -Path C:\Users\b41395\proj\ssl\try_env\build\B41395\imx6sll *exe -Recurse | sort lastwritetime -Descending | select -First 1 | ii 
 }
 
-function Set-profile{
-	vim C:\Users\b41395\my_profile.ps1
-}
-
 set-alias gh get-help
 
 function Connect-Wifi{
 netsh wlan connect name=Freescale
 }
 set-alias wifi Connect-Wifi
+
+function jl{
+    JLink.exe -new_console:s
+}
